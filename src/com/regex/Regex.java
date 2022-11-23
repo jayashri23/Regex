89c996b276ffd9eb9271c15fbe8116cdd7fc0d20
jayashri23 <jsjankar23@gmail.com>
2022-11-23 16:17:41 +0530

    Checking valid last name

--- src/com/regex/Regex.java
+++ src/com/regex/Regex.java
@@ -19,4 +19,18 @@ public class Regex {
         }
         return flag;
     }
+
+
+    //method to check user lastname
+    public static boolean validLastName(String name){
+        boolean flag=false;
+        String regex = "[A-Z][a-z]{3,}";     //3, means above 3 char first name
+        Pattern pattern =Pattern.compile(regex);
+        for (int i=0;i <name.length();i++)
+        {
+            Matcher matcher=pattern.matcher(name);
+            flag =matcher.matches();
+        }
+        return flag;
+    }
 }
--- src/com/regex/RegexMain.java
+++ src/com/regex/RegexMain.java
@@ -4,6 +4,7 @@ package com.regex;
 import java.util.Scanner;
 
 import static com.regex.Regex.validFirstName;
+import static com.regex.Regex.validLastName;
 
 public class RegexMain {
     public static void main(String [] args){
@@ -15,6 +16,16 @@ public class RegexMain {
         }else
         {
             System.out.println("Given first name: " +firstName+ " is not  valid");
+        }
+
+
+        System.out.println("Enter the user's last Name:");
+        String lastName = sc.next();
+        if (validLastName(lastName)){
+            System.out.println("Given last name: " +lastName+" is valid");
+        }else
+        {
+            System.out.println("Given last name: " +lastName+ " is not valid");
         }
     }
 }
